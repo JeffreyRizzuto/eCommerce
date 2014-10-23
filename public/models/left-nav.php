02e6860a14e336375c598f86d623366da398c997
Jeffrey Rizzuto <jeffreyrizzuto@gmail.com>
2014-10-23 18:50:49 -0500

    test
    fixed the git thing

--- public/models/left-nav.php
+++ public/models/left-nav.php
@@ -2,7 +2,7 @@
     <p class="lead">CSBS</p>
     <div class="list-group">
         <a href="../greg_test_page.php" class="list-group-item">Greg's Test Page</a>
-        <a href="index.php" class="list-group-item">--Unused--</a>
+        <a href="index.php" class="list-group-item">Unused</a>
         <a href="#" class="list-group-item">Leave me alone</a>
     </div>
 </div>
