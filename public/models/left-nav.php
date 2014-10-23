900806eb3ba691be2aed854bc51409c89dfd1f9d
ggomez90 <ggomez903@gmail.com>
2014-10-23 17:46:33 -0500

    The ultimate test.

--- public/models/left-nav.php
+++ public/models/left-nav.php
@@ -1,8 +1,8 @@
 <div class="col-md-3">
     <p class="lead">CSBS</p>
     <div class="list-group">
-        <a href="../greg_test_page.php" class="list-group-item">Gregs Test Page</a>
-        <a href="../greg_test_page.php" class="list-group-item">Gregs Test Page</a>
-        <a href="../greg_test_page.php" class="list-group-item">Gregs Test Page</a>
+        <a href="../greg_test_page.php" class="list-group-item">Greg's Test Page</a>
+        <a href="#" class="list-group-item">Unused</a>
+        <a href="#" class="list-group-item">Leave me alone</a>
     </div>
 </div>
