4fcb9acb9179516633232f34f7f5031644876ffa
ggomez90 <ggomez903@gmail.com>
2014-10-23 22:04:02 -0500

    Added cart.php

---
+++ public/cart.php
@@ -0,0 +1,26 @@
+<?php
+require_once("models/config.php");
+if(!isUserLoggedIn()) { header("Location: login.php"); die(); }
+
+echo "Someone else can make this pretty. I am just doin my work<br>"
+
+$euser = $_SESSION["eUser"];
+
+$cartInfo[] = $euser->getCartInfo();
+$total = $euser->getCartTotal();
+
+echo "<html>";
+foreach ($cartInfo as $c) {
+	echo "
+	OID: 		$c['oid']<br>
+	ISBN:		$c['isbn']<br>
+	Quantity:	$c['qty']<br>
+	Date Added:	$c['date']<br><hr>";
+}
+
+echo "Subtotal: $$total<br>";
+
+
+echo "</html>"
+
+?>
