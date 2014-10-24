a0d9b648708081af543de95cd02ade59c91e1efa
Jeff Rizzuto <jeffreyrizzuto@gmail.com>
2014-10-23 20:39:46 -0500

    test something

--- public/left-nav.php
+++
@@ -1,8 +0,0 @@
-<div class="col-md-3">
-    <p class="lead">CSBS</p>
-    <div class="list-group">
-        <a href="../greg_test_page.php" class="list-group-item">Gregs Test Page</a>
-        <a href="../greg_test_page.php" class="list-group-item">Gregs Test Page</a>
-        <a href="../greg_test_page.php" class="list-group-item">Gregs Test Page</a>
-    </div>
-</div>
