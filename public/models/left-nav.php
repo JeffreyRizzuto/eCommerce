9007564b1e99f2e1790614bb17da60fda3415216
ggomez90 <ggomez903@gmail.com>
2014-10-23 21:08:21 -0500

    T

--- public/models/left-nav.php
+++ public/models/left-nav.php
@@ -2,7 +2,7 @@
     <p class="lead">CSBS</p>
     <div class="list-group">
         <a href="../greg_test_page.php" class="list-group-item">Greg's Test Page</a>
-        <a href="login.php" class="list-group-item">Okay, click</a>
+        <a href="login.php" class="list-group-item">Okay, clicky click</a>
         <a href="#" class="list-group-item">Idaho?</a>
     </div>
 </div>
