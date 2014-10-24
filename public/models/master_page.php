ad31bc81093ab6536e40588fe086bcb3957c25d6
ggomez90 <ggomez903@gmail.com>
2014-10-24 15:16:51 -0500

    Some minor changes to title.

--- public/models/master_page.php
+++ public/models/master_page.php
@@ -14,7 +14,7 @@ if (!securePage($_SERVER['PHP_SELF'])){die();}
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Shop Homepage - Start Bootstrap Template</title>
+    <title>CSBS - Computer Science B***S***</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
