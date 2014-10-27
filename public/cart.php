ce3ba1c4b70efad3ec2f0c3956f673af39ad451a
Jeff Rizzuto <jeffreyrizzuto@gmail.com>
2014-10-27 16:50:08 -0500

    revert

--- public/cart.php
+++ public/cart.php
@@ -1,63 +1,6 @@
 <?php
 require_once("models/config.php");
 if(!isUserLoggedIn()) { header("Location: login.php"); die(); }
-require_once("models/master_page.php");
-?>
-
-<div class="col-md-5 col-sm-12">
-    <div class="bigcart"></div>
-    <h1>Your shopping cart</h1>
-    <p>
-        Something Something shopping cart
-    </p>
-</div>
-
-<div class="col-md-7 col-sm-12 text-left">
-    <ul>
-        <li class="row list-inline columnCaptions">
-            <span>QTY</span>
-            <span>ITEM</span>
-            <span>Price</span>
-        </li>
-        <li class="row">
-            <span class="quantity">1</span>
-            <span class="itemName">Item Name</span>
-            <span class="popbtn"><a class="arrow"></a></span>
-            <span class="price">$49.95</span>
-        </li>
-        <li class="row">
-            <span class="quantity">1</span>
-            <span class="itemName">Item Name</span>
-            <span class="popbtn"><a class="arrow"></a></span>
-            <span class="price">$5.00</span>
-        </li>
-        <li class="row">
-            <span class="quantity">1</span>
-            <span class="itemName">Item Name</span>
-            <span class="popbtn"><a class="arrow"></a></span>
-            <span class="price">$919.99</span>
-        </li>
-        <li class="row">
-            <span class="quantity">1</span>
-            <span class="itemName">Item Name</span>
-            <span class="popbtn"><a class="arrow"></a></span>
-            <span class="price">$269.45</span>
-        </li>
-        <li class="row">
-            <span class="quantity">1</span>
-            <span class="itemName">Item Name</span>
-            <span class="popbtn"  data-parent="#asd" data-toggle="collapse" data-target="#demo"><a class="arrow"></a></span>
-            <span class="price">$450.00</span>
-        </li>
-        <li class="row totals">
-            <span class="itemName">Total:</span>
-            <span class="price">$Something/span>
-            <span class="order"> <a class="text-center">ORDER</a></span>
-        </li>
-    </ul>
-</div>
-
-<?php
 
 $euser = $_SESSION["euser"];
 
@@ -78,8 +21,6 @@ foreach ($cartInfo as $c) {
 
 echo "Subtotal: $total</html>";
 
-echo"</body>";
-<!-- Footer-->
-require 'models/footer.php';
+
 
 ?>
