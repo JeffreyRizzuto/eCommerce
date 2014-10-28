2ce127eddae743ac2f43e759b3960aa070562503
UTSA <utsa@raspberrypi.(none)>
2014-10-28 12:28:28 -0500

    okay

--- public/models/item_page.php
+++ public/models/item_page.php
@@ -1,6 +1,6 @@
 <?php
-require_once("models/config.php");
-require_once("models/master_page.php");
+require_once("config.php");
+require_once("master_page.php");
 
 echo "TEST PAGE PLEASE IGNORE";
 /*
