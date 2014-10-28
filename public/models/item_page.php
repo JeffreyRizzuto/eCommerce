01ff1fa570b8f6f93e671da839bd17c3f13f9790
ggomez903 <ggomez903@gmail.com>
2014-10-28 11:31:20 -0500

    got rid of securePage requirement

--- public/models/item_page.php
+++ public/models/item_page.php
@@ -1,6 +1,6 @@
 <?php
 require_once("models/config.php");
-if (!securePage($_SERVER['PHP_SELF'])){die();}
+//if (!securePage($_SERVER['PHP_SELF'])){die();}
 require_once("models/master_page.php");
 
 
