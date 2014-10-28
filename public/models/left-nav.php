4eb3328a3cfbbcd437aa84d48a427197ac0c393d
ggomez90 <ggomez903@gmail.com>
2014-10-28 13:57:10 -0500

    Removed links in left-nav.php

--- public/models/left-nav.php
+++ public/models/left-nav.php
@@ -1,8 +1,8 @@
 <div class="col-md-3">
     <p class="lead">CSBS</p>
     <div class="list-group">
-        <a href="greg_test_search2.php" class="list-group-item">Test Search Page 2</a>
-        <a href="greg_test_search.php" class="list-group-item">Greg's Test Search Page</a>
-        <a href="#" class="list-group-item">Idaho?</a>
+        <a href="#" class="list-group-item">Unused</a>
+        <a href="#" class="list-group-item">Unused</a>
+        <a href="#" class="list-group-item">Unused</a>
     </div>
 </div>
