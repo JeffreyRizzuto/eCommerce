84d44894f7e9b884d55423fe6a2ac005802e4d34
rmv693 <artemusxi@gmail.com>
2014-10-29 17:28:09 -0500

    homepage banner

--- public/homepage.php
+++ public/homepage.php
@@ -16,19 +16,23 @@
                     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                         <ol class="carousel-indicators">
                             <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <!--
                             <li data-target="#carousel-example-generic" data-slide-to="1"></li>
                             <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                            -->
                         </ol>
                         <div class="carousel-inner">
                             <div class="item active">
-                                <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                <img class="slide-image" src="../images/BookSale_banner.png" style="width: 800%; height: 300%;" alt="">
                             </div>
+                            <!--
                             <div class="item">
                                 <img class="slide-image" src="http://placehold.it/800x300" alt="">
                             </div>
                             <div class="item">
                                 <img class="slide-image" src="http://placehold.it/800x300" alt="">
                             </div>
+                            -->
                         </div>
                         <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
                             <span class="glyphicon glyphicon-chevron-left"></span>
