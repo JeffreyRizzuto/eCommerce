ed444e9fc4dfbb5af37fdc63a97fca41080e111b
Jeff Rizzuto <jeffreyrizzuto@gmail.com>
2014-10-28 22:07:55 -0500

    add validation for apartment number

--- public/register.php
+++ public/register.php
@@ -74,7 +74,7 @@ if(!empty($_POST))
         $errors[] = lang("ACCOUNT_STREET_INVALID");
     }
     //street number
-    if(!ctype_digit($streetnumber))
+    if(!ctype_digit($apartmentnumber) || empty($apartmentnumber))
     {
         $errors[] = lang("ACCOUNT_STREET_NUMBER_INVALID");
     }
@@ -242,7 +242,7 @@ if(!empty($_POST))
                     <div class="col-xs-12 col-sm-1 col-md-1">
                         <div class="form-group">
                             <!-- Display Name -->
-                            <label class="control-label">Apartment Number</label>
+                            <label class="control-label">*Apartment Number</label>
                             <div class="controls">
                                 <input type="text"  name="apartmentnumber" placeholder="" class="input-xlarge">
                             </div>
