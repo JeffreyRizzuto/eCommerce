cbba14b95e12a414962a199b76261ef81494debc
ggomez90 <ggomez903@gmail.com>
2014-10-29 16:09:51 -0500

    yes

--- public/models/class.neweuser.php
+++ public/models/class.neweuser.php
@@ -277,6 +277,8 @@ class EUser {
     function updateQuantity($newQty, $isbn) {
         global $myQuery;
 
+        echo "newQty: $newQty<br>ISBN: $isbn<br>";
+
         $query = "UPDATE `book_order` SET `qty`=$newQty WHERE `oid` = ? AND `ISBN` = ?";
         $stmt = $myQuery->prepare($query);
         $stmt->bind_param("ii", $this->cart, $isbn);
