5a5cb94a072bd99d9072b0d0e22c7bdda479ce97
ggomez90 <ggomez903@gmail.com>
2014-10-31 13:33:10 -0500

    Updates to account nav bar

--- public/models/account-nav.php
+++ public/models/account-nav.php
@@ -1,5 +1,5 @@
 <div class="col-md-3">
-    <p class="lead">CSBS</p>
+    <p class="lead">Account Management</p>
     <div class="list-group">
         <a href="../past_orders.php" class="list-group-item">Past Orders</a>
         <a href="#" class="list-group-item">Unused</a>
