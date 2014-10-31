10eae7ed31db865938aceae37475f9a4532f3dcf
ggomez90 <ggomez903@gmail.com>
2014-10-31 11:45:16 -0500

    Fixed it!

--- public/past_orders.php
+++ public/past_orders.php
@@ -12,13 +12,11 @@ var_dump($info);
 foreach($info as $inf) {
 	echo "Order #:			".$inf['oid']."<br>";	
 	if(is_array($inf)) {
-		//foreach($inf as $i) {
-   			echo "
-      			ISBN:			".$inf['o_inf']['isbn']."<br>
-      			Quantity:		".$inf['o_inf']['qty']."<br>
-      			Date Added:		".$inf['o_inf']['date']."<br>
-   			";
-   		//}//end of inner foreach
+		echo "
+      		ISBN:			".$inf['o_inf']['isbn']."<br>
+      		Quantity:		".$inf['o_inf']['qty']."<br>
+      		Date Added:		".$inf['o_inf']['date']."<br>
+   		";
 	}//end of if 
 	echo "Total Price: $".$inf['price']."<br><hr>";
 }//end of outer foreach
