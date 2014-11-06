31d203828c83d77fb78b3a8cd0fd68b4a0ddef8d
rmv693 <artemusxi@gmail.com>
2014-11-06 17:05:20 -0600

    test register

--- public/testregister.php
+++ public/testregister.php
@@ -151,7 +151,7 @@ if(!empty($_POST))
 			<div class="row">
 				<div class="col-xs-12 col-sm-6 col-md-6">
 					<div class="form-group">
-                        <input type="text" name="firstname" id="firstname" class="form-control input-lg" placeholder="First Name" tabindex="1">
+					<input type="text" name="firstname" id="firstname" class="form-control input-lg" placeholder="First Name" tabindex="1">
 					</div>
 				</div>
 				<div class="col-xs-12 col-sm-6 col-md-6">
@@ -224,7 +224,7 @@ if(!empty($_POST))
 				</div>
 			</div>
 			
-			<div class="row">
+			<div class="form-group">
 				<div class="col-xs-8 col-sm-9 col-md-9">
 					 By clicking <strong class="label label-primary">Register</strong>, you agree to the <a href="#" data-toggle="modal" data-target="#t_and_c_m">Terms and Conditions</a> set out by this site, including our Cookie Use.
 				</div>
