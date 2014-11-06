96ce334ec9ab27e94026494d13481de3d3bbcaf1
Jeff Rizzuto <jeffreyrizzuto@gmail.com>
2014-11-05 23:37:10 -0600

    undo to see if it works

--- public/models/navbar.php
+++ public/models/navbar.php
@@ -12,22 +12,22 @@
                 print $count;
                     if(isUserLoggedIn()){
                         echo "<li><a href='account.php'>Account</a></li>";
-                        if($count){
+                        #if($count){
                             echo "<li><a href='cart.php'>Cart</a><span class='badge'>$count</span></li>";
-                        }else{
+                       # }else{
                             echo "<li><a href='cart.php'>Cart</a></li>";
-                        }
+                        #}
                         echo"<li><a href='logout.php'>Logout</a></li>";
                     }
                     else{
                         echo"
                         <li><a href='login.php'>Login</a></li>
                         <li><a href='register.php'>Register</a></li>";
-                        if($count){
+                        #if($count){
                         echo"<li><a href='cart.php'>Cart</a><span class='badge'>$count</span></li>";
-                        }else{
+                        #}else{
                             echo"<li><a href='cart.php'>Cart</a></li>";
-                        }
+                        #}
                     }
                 ?>
             </ul>
