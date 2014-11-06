699b6444de9a9ed608d402da2dbaaddbd55e5faf
ggomez903 <ggomez903@gmail.com>
2014-11-06 16:12:43 -0600

    Removed the unused links

--- public/models/left-nav.php
+++ public/models/left-nav.php
@@ -1,8 +1,10 @@
 <div class="col-md-3">
     <p class="lead">CSBS</p>
+    <!--
     <div class="list-group">
         <a href="#" class="list-group-item">Unused</a>
         <a href="#" class="list-group-item">Unused</a>
         <a href="#" class="list-group-item">Unused</a>
     </div>
+    -->
 </div>
