e8dc459725dd8a6090c496dc953954dca82aeb22
rmv693 <artemusxi@gmail.com>
2014-11-06 18:19:19 -0600

    test register

--- public/testregister.php
+++ public/testregister.php
@@ -238,7 +238,7 @@ if(!empty($_POST))
 			
 			<hr class="colorgraph">
 			<div class="row">
-				<div class="col-xs-12 col-md-6"><input type="submit" value="Register" class="btn btn-primary btn-block btn-lg"></div>
+				<div class="col-xs-12 col-md-6"><button class="btn btn-success">Register</button></div>
 				<div class="col-xs-12 col-md-6"><a href="login.php" class="btn btn-success btn-block btn-lg">Sign In</a></div>
 			</div>
 		</form>
