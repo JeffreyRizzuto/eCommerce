7b2d59eb0bad38856d38b55bbb6ad54c2259d90f
Jeff Rizzuto <jeffreyrizzuto@gmail.com>
2014-11-06 19:32:26 -0600

    hopefully fixed count cart function

--- public/models/class.euser.php
+++ public/models/class.euser.php
@@ -300,11 +300,13 @@ class EUser {
     function getCartSize(){
         global $myQuery;
 
-        $stmt = $myQuery->prepare("SELECT COUNT('oid' = ?) FROM `book_order` ");
+        $stmt = $myQuery->prepare("SELECT *  FROM 'book_order' WHERE 'oid' = ?");
         $stmt->bind_param("i", $this->cart);
         $stmt->execute();
-        $stmt->bind_result($count);
-        $stmt->fetch();
+        $count= 0;
+        while($stmt->fetch()){
+            $count++;
+        }
         $stmt->close();
 
         return $count;
