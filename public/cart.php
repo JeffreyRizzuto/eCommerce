f09483218c2138c92253a7f138ab5bc53f13acf1
ggomez90 <ggomez903@gmail.com>
2014-12-02 16:51:16 -0600

    testing cart

--- public/cart.php
+++ public/cart.php
@@ -36,7 +36,9 @@ $euser = $_SESSION["euser"];
                         <li>Quantity: <input type='text' id='qty' name='qty' value='".$c['qty']."'/></span><input type='Submit' value='Update Quantity' /></li>
                         <li>Date Added: " . $c['date'] . "</span></li>
                         <input type='hidden' id='qty_isbn' name='qty_isbn' value='".$c['isbn']."'/>
-                        <li>Price: $".getPrice($c['isbn'])."</li>
+                        </form><li>Price: $".getPrice($c['isbn'])."</li>
+                        <form name='deleteFromCart' action='".$_SERVER['PHP_SELF']."' method='post'><li><input type='Submit' value='Delete' /></li>
+                        <input type='hidden' id='del_isbn' name='del_isbn' value='".$c['isbn']."'/>
                         </form>
                     </ul>
                     </div>
