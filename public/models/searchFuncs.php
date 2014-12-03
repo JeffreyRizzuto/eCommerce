8ad317fc976d96ec29a2d7b7411a25ab64e4d72b
ggomez903 <ggomez903@gmail.com>
2014-12-02 20:44:23 -0600

    :D?

--- public/models/searchFuncs.php
+++ public/models/searchFuncs.php
@@ -78,9 +78,8 @@ function searchByCourse($course) {
 function searchByAuthor($searchAuthor) {
 	global $myQuery;
 
-	$search_string = '%'.$searchAuthor.'%';
-	$stmt = $myQuery->prepare("SELECT * FROM `books` WHERE ? REGEXP `author`");
-	$stmt->bind_param("s", $search_string);
+	$stmt = $myQuery->prepare("SELECT * FROM `books` WHERE `author` REGEXP ?");
+	$stmt->bind_param("s", $searchAuthor);
 	$stmt->execute();
 	$stmt->bind_result($course, $cat, $isbn, $title, $edition, $author, $type, $price, $details, $publisher, $quantity);
 	while($stmt->fetch()) {
