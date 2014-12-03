14467adb2b2f32cc44c3c5ebb27e180b5b320eca
Jeff Rizzuto <jeffreyrizzuto@gmail.com>
2014-12-03 12:40:28 -0600

    style and make screen readable past orders page

--- public/past_orders.php
+++ public/past_orders.php
@@ -10,17 +10,23 @@ $info = $euser->getPastOrders();
 //var_dump($info);
 
 if(is_null($info)) {
-	echo "You have no past orders.<br>";
+	echo "<h1>You have no past orders.</h1>";
 } else {
 	foreach($info as $inf) {
-		echo "Order #:			".$inf['oid']."<br><br>";	
+		echo "<h1>Order #:			".$inf['oid']."</h1><br><br>";
 		foreach($inf['o_inf'] as $i) {
    			echo "
-                             Title:                     ".$i['title']."<br>
-      			ISBN:			".$i['isbn']."<br>
-      			Quantity:		".$i['qty']."<br>
-      			Date Added:		".$i['date']."<br>
-                             Price:                     ".$i['book_price']."<br><br>
+   			<div class='panel panel-default'>
+                <div class='panel-body'>
+                    <ul class='pull-left' style='list-style-type:none'>
+                        <li>Title:          ".$i['title']."</li>
+                        <li>ISBN:			".$i['isbn']."</li>
+                        <li>Quantity:		".$i['qty']."</li>
+                        <li>Date Added:		".$i['date']."</li>
+                        <li>Price:          ".$i['book_price']."</li>
+                    </ul>
+                </div>
+            </div>
    			";
    		}//end of inner foreach
 		echo "Total Price: $".$inf['price']."<br><hr>";
