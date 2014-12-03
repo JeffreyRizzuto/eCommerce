c86d34964b37004f7aacb82cdeebb6b40f8a365e
Jeff Rizzuto <jeffreyrizzuto@gmail.com>
2014-12-03 13:09:27 -0600

    lets see how integrating the past orders page with the account look works

--- public/models/account-nav.php
+++ public/models/account-nav.php
@@ -1,8 +1,9 @@
 <div class="col-md-3">
     <p class="lead">Account Management</p>
     <div class="list-group">
+
+        <a href="../account.php" class="list-group-item">Account Home</a>
         <a href="../past_orders.php" class="list-group-item">Past Orders</a>
-        <a href="#" class="list-group-item">Unused</a>
-        <a href="#" class="list-group-item">Unused</a>
+        <a href="#" class="list-group-item">Change Account Info</a>
     </div>
 </div>
--- public/past_orders.php
+++ public/past_orders.php
@@ -8,31 +8,49 @@ $euser = $_SESSION['euser'];
 $info = $euser->getPastOrders();
 
 //var_dump($info);
-
-if(is_null($info)) {
-	echo "<h3>You have no past orders.</h3>";
-} else {
-	foreach($info as $inf) {
-		echo "
-            <div class='panel panel-default col-xs-12 col-sm-6 col-md-6'>
-                <div class='panel-body'>
-                    <h3>Order #:			".$inf['oid']."</h3>
-		            <ul class='list-group'>";
-                    foreach($inf['o_inf'] as $i) {
-                        echo "
-                        <li class='list-group-item'>
-                            Title:          ".$i['title']."<br>
-                            ISBN:			".$i['isbn']."<br>
-                            Quantity:		".$i['qty']."<br>
-                            Date Added:		".$i['date']."<br>
-                            Price:          ".$i['book_price']."<br>
-                        </li>
-                        ";
-                    }//end of inner foreach
-                    echo "<h4>Total Price: $".$inf['price']."</h4>
-                                </ul>
-                            </div>
-                        </div>";
-	}//end of outer foreach
-}//end of else
 ?>
+
+
+<div class='container center-block'>
+    <div id='top'><div id='logo'></div></div>
+        <div id='content'>
+            <h2>Account</h2>
+        </div>
+         <div id='main'>
+             <?php
+            require_once("models/account-nav.php");
+            ?>
+    </div>
+</div>
+
+<?php
+    if(is_null($info)) {
+        echo "<h3>You have no past orders.</h3>";
+    } else {
+        foreach($info as $inf) {
+            echo "
+
+                <div class='panel panel-default'>
+                    <div class='panel-body'>
+                        <h3>Order #:			".$inf['oid']."</h3>
+                        <ul class='list-group'>";
+                        foreach($inf['o_inf'] as $i) {
+                            echo "
+                            <li class='list-group-item'>
+                                Title:          ".$i['title']."<br>
+                                ISBN:			".$i['isbn']."<br>
+                                Quantity:		".$i['qty']."<br>
+                                Date Added:		".$i['date']."<br>
+                                Price:          ".$i['book_price']."<br>
+                            </li>
+                            ";
+                        }//end of inner foreach
+                        echo "<h4>Total Price: $".$inf['price']."</h4>
+                                    </ul>
+                                </div>
+                            </div>";
+        }//end of outer foreach
+    }//end of else
+?>
+    <!-- Footer-->
+    <?php   require 'models/footer.php';  ?>
