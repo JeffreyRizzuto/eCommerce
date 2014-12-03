01c6e35755c7330a7f18f65a097bee72e724c719
Jeff Rizzuto <jeffreyrizzuto@gmail.com>
2014-12-03 12:48:05 -0600

    new styling for past orders

--- public/past_orders.php
+++ public/past_orders.php
@@ -10,27 +10,29 @@ $info = $euser->getPastOrders();
 //var_dump($info);
 
 if(is_null($info)) {
-	echo "<div class='panel panel-default'>
-                <div class='panel-body'>
-                    <ul class='pull-left' style='list-style-type:none'>
-                        <h1>You have no past orders.</h1>";
+	echo "<h2>You have no past orders.</h2>";
 } else {
 	foreach($info as $inf) {
-		echo "<h1>Order #:			".$inf['oid']."</h1><br><br>";
-		foreach($inf['o_inf'] as $i) {
-   			echo "
-
-                        <li>Title:          ".$i['title']."</li>
-                        <li>ISBN:			".$i['isbn']."</li>
-                        <li>Quantity:		".$i['qty']."</li>
-                        <li>Date Added:		".$i['date']."</li>
-                        <li>Price:          ".$i['book_price']."</li>
-   			";
-   		}//end of inner foreach
-		echo "Total Price: $".$inf['price']."
-		            </ul>
-                </div>
-            </div>";
+		echo "
+            <div class='panel panel-default'>
+                <div class='panel-body'>
+                    <h4>Order #:			".$inf['oid']."</h4>
+		            <ul class='list-group'>";
+                    foreach($inf['o_inf'] as $i) {
+                        echo "
+                        <li class='list-group-item'>
+                            Title:          ".$i['title']."
+                            ISBN:			".$i['isbn']."
+                            Quantity:		".$i['qty']."
+                            Date Added:		".$i['date']."
+                            Price:          ".$i['book_price']."
+                        </li>
+                        ";
+                    }//end of inner foreach
+                    echo "Total Price: $".$inf['price']."
+                                </ul>
+                            </div>
+                        </div>";
 	}//end of outer foreach
 }//end of else
 ?>
